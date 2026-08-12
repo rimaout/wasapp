607ca5c8a8f31e4758454bbe0b5c2f7911cad9b9
Matteo <116072651+rimaout@users.noreply.github.com>
2026-08-12 13:50:40 +0200

    fix(frontend): add missing prefix bearer token in axios auth header

--- webui/src/services/axios.js
+++ webui/src/services/axios.js
@@ -10,7 +10,7 @@ const instance = axios.create({
 instance.interceptors.request.use(config => {
 	const token = getToken();
 	if (token) {
-		config.headers.Authorization = token;
+		config.headers.Authorization = 'Bearer ' + token;
 	}
 	return config;
 });
